ac5e12ebf4d9e850e9d1887d686a6cbb0dade4b9
Alex Kor <vlc361358@gmail.com>
2021-09-12 12:00:54 +0200

    new class for configuration

--- rangebar/configuration.py
+++ rangebar/configuration.py
@@ -4,10 +4,19 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     db: str = 'postgresql'
     module: str = 'psycopg2'
-    db_user: str = 'userbinance'
+    username: str = 'userbinance'
     base: str = 'candlesticks'
     host: str = 'localhost'
     port: int = 5432
+    table = 'range_bar'
 
 
-settings = Settings()
+class SettingsWS(BaseSettings):
+    ws: str = 'ws'
+    wss: str = 'wss'
+    binance: str = 'fstream.binance.com/ws'
+    stream: str = 'aggTrade'
+
+
+ws_settings = SettingsWS()
+db_settings = Settings()
