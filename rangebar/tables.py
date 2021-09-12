f9ec66337dbbe7a0f672ff783e0df2d7008ef88e
Alex Kor <vlc361358@gmail.com>
2021-09-12 14:56:49 +0200

    update table

--- rangebar/tables.py
+++ rangebar/tables.py
@@ -1,11 +1,11 @@
 from sqlalchemy import Column, Integer, BigInteger, Float
 from sqlalchemy.ext.declarative import declarative_base
-
+from configuration import db_settings as db
 Base = declarative_base()
 
 
 class RangeBar(Base):
-    __tablename__ = 'range_bar'
+    __tablename__ = db.table
     id = Column(Integer, primary_key=True, autoincrement=True)
     timestamp = Column(BigInteger)
     volume = Column(Float)
